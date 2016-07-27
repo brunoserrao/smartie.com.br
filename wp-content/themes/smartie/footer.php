d2e6a6de393482a2acaffcb8f158b7e5fba88e29
Bruno Serrão <brunosserrao@gmail.com>
2016-07-26 23:43:16 -0300

    Fixed Cart itens count

--- wp-content/themes/smartie/footer.php
+++ wp-content/themes/smartie/footer.php
@@ -11,6 +11,20 @@
 		</div><!-- .col-full -->
 	</div><!-- #content -->
 
+	<?php if (!is_cart() and !is_checkout()) { ?>
+		<?php $cart_count = WC()->cart->get_cart_contents_count(); ?>
+		<?php if($cart_count) { ?>
+			<div id="cart-count">
+				<a href="<?php echo WC()->cart->get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>">
+					<span class="icon"></span>
+					<span class="itens-count">
+						<?php echo  sprintf("%02d",$cart_count); ?>
+					</span>
+				</a>
+			</div>
+		<?php } ?>
+	<?php }?>
+
 	<?php do_action( 'storefront_before_footer' ); ?>
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
@@ -21,7 +35,7 @@
 			 * @hooked storefront_footer_widgets - 10
 			 * @hooked storefront_credit - 20
 			 */
-			do_action( 'storefront_footer' ); ?>
+			// do_action( 'storefront_footer' ); ?>
 
 		</div><!-- .col-full -->
 	</footer><!-- #colophon -->
