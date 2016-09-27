330a400f1f50efcbb201abc14d8b85da67a676b0
Bruno Serrão <brunosserrao@gmail.com>
2016-09-27 16:48:35 -0300

    Emails hook

--- wp-content/themes/smartie/functions.php
+++ wp-content/themes/smartie/functions.php
@@ -92,7 +92,7 @@ function themeprefix_back_to_store() {
 /**
  * Code goes in functions.php or a custom plugin.
  */
-add_action( 'woocommerce_email', 'unhook_those_pesky_emails' );
+add_action( 'woocommerce_email', 'unhook_those_pesky_emails', 99 );
 function unhook_those_pesky_emails( $email_class ) {
 	/**
 	 * Hooks for sending emails during store events
@@ -110,12 +110,14 @@ function unhook_those_pesky_emails( $email_class ) {
 	// remove_action( 'woocommerce_order_status_failed_to_on-hold_notification', array( $email_class->emails['WC_Email_New_Order'], 'trigger' ) );
 	
 	// Processing order emails
-	// remove_action( 'woocommerce_order_status_pending_to_processing_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
-	// remove_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
+	remove_action( 'woocommerce_order_status_pending_to_processing_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
+	remove_action( 'woocommerce_order_status_pending_to_on-hold_notification', array( $email_class->emails['WC_Email_Customer_Processing_Order'], 'trigger' ) );
 	
 	// Completed order emails
 	// remove_action( 'woocommerce_order_status_completed_notification', array( $email_class->emails['WC_Email_Customer_Completed_Order'], 'trigger' ) );
 		
 	// Note emails
 	// remove_action( 'woocommerce_new_customer_note_notification', array( $email_class->emails['WC_Email_Customer_Note'], 'trigger' ) );
+
+	return $$email_class;
 }
