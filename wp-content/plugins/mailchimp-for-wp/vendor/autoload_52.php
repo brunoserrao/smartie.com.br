142d053a6c9e26cabfc89f5ed7739388b4b01b4b
Bruno Serrão <brunosserrao@gmail.com>
2017-12-22 21:39:19 +0100

    MailChimp for WordPress

--- wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit541c0dc3d2f08af0d23e4f44958746d3::getLoader();
+return ComposerAutoloaderInita1ed0494cfc3f6407584f55896c35bcf::getLoader();
